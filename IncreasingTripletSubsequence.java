5ebf2f5cd7d7663ae00b927e53313463314513b4
AllyWang <liwang1025@gmail.com>
2016-02-17 00:44:22 +0800

    leetCode 334. triple increasing numbers

---
+++ IncreasingTripletSubsequence.java
@@ -0,0 +1,41 @@
+/*Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+
+Formally the function should:
+
+    Return true if there exists i, j, k
+    such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false. 
+
+Your algorithm should run in O(n) time complexity and O(1) space complexity.
+
+Examples:
+Given [1, 2, 3, 4, 5],
+return true.
+
+Given [5, 4, 3, 2, 1],
+return false. 
+*/
+
+
+
+public class IncreasingTripletSubsequence {
+	public static boolean increasingTriplet(int[] nums) {
+        if(nums.length <= 0) return false;
+        int left = Integer.MAX_VALUE, right = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] <= left){
+                left = nums[i];
+            }else if(nums[i] <= right){
+                right = nums[i];
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+	public static void main(String[] args) {
+           boolean p = increasingTriplet(new int[] {1, 3, 5, 7, 9, 12});
+           System.out.println("Result: " + p);
+	}	
+
+}
