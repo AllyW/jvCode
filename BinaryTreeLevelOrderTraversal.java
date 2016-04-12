65f957d3e833c444f4ab3f32d385b8b905efa00e
AllyWang <liwang1025@gmail.com>
2016-04-12 22:40:53 +0800

    binary tree level traversal

---
+++ BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,93 @@
+/*
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its level order traversal as:
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+*/
+
+
+import java.io.*;
+import java.util.*;
+public class BinaryTreeLevelOrderTraversal {
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null) return res;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int s = q.size();
+            List<Integer> a = new ArrayList<Integer>();
+            for(int i = 0; i < s; i++){
+                TreeNode t = q.poll();
+                a.add(t.val);
+                if(t.left != null) q.offer(t.left);
+                if(t.right!= null) q.offer(t.right);
+            }
+            res.add(a);
+        }
+        return res;
+    }
+
+	public static void main(String[] args) { 
+		TreeNode root = new TreeNode(1);
+		TreeNode node2 = new TreeNode(2);
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node5 = new TreeNode(5);
+		TreeNode node6 = new TreeNode(6);
+		TreeNode node7 = new TreeNode(7);
+		TreeNode node8 = new TreeNode(8);
+		TreeNode node9 = new TreeNode(9);
+		TreeNode node10 = new TreeNode(10);
+		TreeNode node11 = new TreeNode(11);
+		root.left = node2;
+		root.right = node3;
+		node2.left = node4;
+		node2.right = node5;
+		node3.left = node6;
+		node3.right = node7;
+		node4.left = node8;
+		node4.right = node9;
+		node5.left = node10;
+		node5.right = node11;
+		node6.left =  null;
+		node6.right = null;
+		node7.left = null;
+		node7.right = null;
+		node8.left = null;
+		node8.right = null;
+		node9.left = null;
+		node9.right = null;
+		node10.left = null;
+		node10.right = null;
+		node11.left = null;
+		node11.right = null;
+		List<List<Integer>> res = levelOrder(root);
+		System.out.println("The level order:");
+		for(int i = 0; i < res.size(); i++){
+			System.out.println(res.get(i));
+		}
+		return;
+	}	
+}
